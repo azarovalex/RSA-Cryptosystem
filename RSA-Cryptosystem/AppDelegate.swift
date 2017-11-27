a86366b4127283952ea7440ca4572bc4f41af901
azarovalex <alex@azarov.by>
2017-11-27 14:03:52 +0300

    Initial Commit

---
+++ RSA-Cryptosystem/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSA-Cryptosystem
+//
+//  Created by Alex Azarov on 27/11/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RSA-Cryptosystem/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSA-Cryptosystem
+//
+//  Created by Alex Azarov on 27/11/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
