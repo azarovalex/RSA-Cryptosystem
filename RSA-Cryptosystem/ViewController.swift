f69196ecfd96bab8435e75aea207e933dfb5b036
azarovalex <alex@azarov.by>
2017-11-27 22:40:15 +0300

    Generate open key from primes p, q and close key d.

--- RSA-Cryptosystem/ViewController.swift
+++ RSA-Cryptosystem/ViewController.swift
@@ -8,24 +8,119 @@
 
 import Cocoa
 
+func dialogError(question: String, text: String) {
+    let alert = NSAlert()
+    alert.messageText = question
+    alert.informativeText = text
+    alert.alertStyle = .critical
+    alert.addButton(withTitle: "OK")
+    alert.runModal()
+}
+
+func browseFile() -> String {
+    let dialog = NSOpenPanel();
+    dialog.title                   = "Choose a file";
+    dialog.showsResizeIndicator    = true;
+    dialog.showsHiddenFiles        = false;
+    dialog.canCreateDirectories    = true;
+    dialog.allowsMultipleSelection = false;
+    
+    if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+        let result = dialog.url
+        
+        if (result != nil) {
+            return result!.path
+        }
+    } else { return "" }
+    return ""
+}
+
+func isPrime(_ number: Int) -> Bool {
+    return number > 1 && !(2..<number).contains { number % $0 == 0 }
+}
+
 class ViewController: NSViewController {
 
-    @IBOutlet weak var p: NSTextField!
-    @IBOutlet weak var q: NSTextField!
-    @IBOutlet weak var d: NSTextField!
+    @IBOutlet weak var p_textbox: NSTextField!
+    @IBOutlet weak var q_textbox: NSTextField!
+    @IBOutlet weak var d_textbox: NSTextField!
     @IBOutlet weak var r_label: NSTextField!
     @IBOutlet weak var euler_label: NSTextField!
     @IBOutlet weak var e_label: NSTextField!
-    @IBOutlet weak var msg: NSScrollView!
-    @IBOutlet weak var cipher: NSScrollView!
+    
+    @IBOutlet var msg: NSTextView!
+    @IBOutlet var cipher: NSTextView!
+    
+    var filePath = ""
+    
+    var msg_bytes = [UInt8]()
+    var ciphered_bytes = [Int]()
+    var p = 0
+    var q = 0
+    var e = 0
+    var d = 0
+    var r = 0
+    var euler = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func encode(_ sender: Any) {
-        r_label.stringValue = "r = \(Int(p.stringValue)! * Int(q.stringValue)!)"
-        
+    func inverse(n: Int, modulus: Int) -> Int{
+        var a = n, b = modulus
+        var x = 0, y = 1, x0 = 1, y0 = 0, q = 0, temp = 0
+        while (b != 0) {
+                q = a / b
+                temp = a % b
+                a = b
+                b = temp
+                temp = x; x = x0 - q * x; x0 = temp;
+                temp = y; y = y0 - q * y; y0 = temp;
+        }
+        if(x0 < 0) { x0 += modulus }
+        return x0
     }
+    
+    @IBAction func Encode(_ sender: Any) {
+        guard Int(p_textbox.stringValue) != nil && isPrime(Int(p_textbox.stringValue)!) else {
+            dialogError(question: "Error!", text: "P is not a prime number.")
+            return
+        }
+        p = Int(p_textbox.stringValue)!
+        guard Int(q_textbox.stringValue) != nil && isPrime(Int(q_textbox.stringValue)!) else {
+            dialogError(question: "Error!", text: "Q is not a prime number.")
+            return
+        }
+        q = Int(q_textbox.stringValue)!
+        guard Int(d_textbox.stringValue) != nil else {
+            dialogError(question: "Error!", text: "D is not a number.")
+            return
+        }
+        d = Int(d_textbox.stringValue)!
+        
+        euler = (p - 1) * (q - 1)
+        euler_label.stringValue = "Euler(r) = \(euler)"
+        r = p * q
+        r_label.stringValue = "r = \(r)"
+        e = inverse(n: d, modulus: euler)
+        e_label.stringValue = "e = \(e)"
+    }
+    
+    @IBAction func OpenFile(_ sender: Any) {
+        let filePath = browseFile()
+        
+        // Represent the file as a sequence of numbers
+        if let data = NSData(contentsOfFile: filePath) {
+            var buffer = [UInt8](repeating: 0, count: data.length)
+            data.getBytes(&buffer, length: data.length)
+            msg_bytes = buffer
+        }
+        
+        msg.string = ""
+        for byte in msg_bytes {
+            msg.string.append(String(byte) + " ")
+        }
+    }
+    
 }
 
